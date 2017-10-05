f4c9eb8b3a97c718eabf68986611b98f34449310
steelrobo <32551265+steelrobo@users.noreply.github.com>
2017-10-06 01:38:10 +0530

    Add files via upload

---
+++ Register.java
@@ -0,0 +1,35 @@
+package mypack;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Register extends ActionSupport{
+private String name,password,email;
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+public String execute(){
+	return "success";
+}
+}
